5f441577aaf7a4b4351a139af5d059fdd12456d1
William Wilhelm <will@wilhelm.com.au>
2020-01-28 22:10:53 +1100

    My CEC patch for Panasonic OLED - fix input stealing

--- packages/security/libgpg-error/package.mk
+++ packages/security/libgpg-error/package.mk
@@ -31,6 +31,7 @@ pre_configure_target() {
   esac
 
   cp $PKG_BUILD/src/syscfg/lock-obj-pub.$GPGERROR_TUPLE.h $PKG_BUILD/src/syscfg/lock-obj-pub.$GPGERROR_TARGET.h
+  sed -i -e "s/namespace/varerrno/g" $PKG_BUILD/src/{Makefile.{in,am},mkstrtable.awk}
 }
 
 post_makeinstall_target() {
